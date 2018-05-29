3e2e828b0562a7f4e22ee2b4be04f962720fbce9
tusmester <tusmester@gmail.com>
2018-05-29 11:21:51 +0200

    Raise version to 1.1.1 and cleanup assembly info files. Add Create nuget package script.

--- src/SenseNet.Client/Properties/AssemblyInfo.cs
+++ src/SenseNet.Client/Properties/AssemblyInfo.cs
@@ -2,11 +2,15 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SenseNet.Client")]
-[assembly: AssemblyDescription("Client library for Sense/Net ECMS")]
+[assembly: InternalsVisibleTo("SenseNet.Client.Tests")]
+
+#if DEBUG
+[assembly: AssemblyTitle("SenseNet.Client (Debug)")]
+#else
+[assembly: AssemblyTitle("SenseNet.Client (Release)")]
+#endif
+
+[assembly: AssemblyDescription("Client library for sensenet")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sense/Net Inc.")]
 [assembly: AssemblyProduct("SenseNet.Client")]
@@ -14,25 +18,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("Sense/Net Inc.")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.1")]
+
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d42c2274-9265-4db9-9f3e-845002f009c7")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
-
-[assembly: InternalsVisibleTo("SenseNet.Client.Tests")]
---
+++ tools/scripts/CreateNuGetPackages.ps1
@@ -0,0 +1,6 @@
+﻿$srcPath = [System.IO.Path]::GetFullPath(($PSScriptRoot + '\..\..\src'))
+
+# delete existing packages
+Remove-Item $PSScriptRoot\*.nupkg
+
+nuget pack $srcPath\SenseNet.Client\SenseNet.Client.csproj -properties Configuration=Release -OutputDirectory $PSScriptRoot
