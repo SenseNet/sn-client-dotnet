280834aa2c1aa43298119554c79515ba305a8d13
tusmester <tusmester@gmail.com>
2018-07-13 13:19:23 +0200

    Netstandard/framework461 (#12)
    * Convert Client project to netstandard and remove local Tools reference.
    * Remove unnecessary nuget package creator script.

--- tools/scripts/CreateNuGetPackages.ps1
+++
@@ -1,6 +0,0 @@
-﻿$srcPath = [System.IO.Path]::GetFullPath(($PSScriptRoot + '\..\..\src'))
-
-# delete existing packages
-Remove-Item $PSScriptRoot\*.nupkg
-
-nuget pack $srcPath\SenseNet.Client\SenseNet.Client.csproj -properties Configuration=Release -OutputDirectory $PSScriptRoot
