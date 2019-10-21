f09d78d841d24a1b718d8df15db6082e5183f36d
tusmester <tusmester@gmail.com>
2019-10-21 12:20:08 +0200

    Add refresh token member.

--- src/SenseNet.Client/ServerContext.cs
+++ src/SenseNet.Client/ServerContext.cs
@@ -8,6 +8,7 @@
         public class AuthenticationInfo
         {
             public string AccessToken { get; set; }
+            public string RefreshToken { get; set; }
         }
 
         /// <summary>
