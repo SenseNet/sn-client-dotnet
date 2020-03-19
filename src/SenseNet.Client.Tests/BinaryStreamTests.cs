161455cecb71c30160b0f99ea4c71b56376aba11
kavics <gyeby5@gmail.com>
2020-03-19 14:04:14 +0100

    Test for oldschool download.

---
+++ src/SenseNet.Client.Tests/BinaryStreamTests.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SenseNet.Client.Tests
+{
+    [TestClass]
+    public class BinaryStreamTests
+    {
+        [ClassInitialize]
+        public static void ClassInitializer(TestContext _)
+        {
+            Initializer.InitializeServer();
+        }
+
+        [TestMethod]
+        public async Task Download_OldSchool()
+        {
+            var content = await Content.LoadAsync("/Root/System/Schema/ContentTypes/GenericContent");
+
+            var request = RESTCaller.GetStreamRequest(content.Id);
+            var response = await request.GetResponseAsync();
+
+            string ctd;
+            using (var stream = response.GetResponseStream())
+            using (var reader = new StreamReader(stream))
+                ctd = reader.ReadToEnd();
+
+            Assert.IsTrue(ctd.Contains("<ContentType name=\"GenericContent\""));
+        }
+    }
+}
