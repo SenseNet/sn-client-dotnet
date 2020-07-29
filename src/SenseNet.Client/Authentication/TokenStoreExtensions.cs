2d015aa3368addd4ca834975a9391e634b663e65
tusmester <tusmester@gmail.com>
2020-07-29 14:56:20 +0200

    Add token store extension method for applications.

---
+++ src/SenseNet.Client/Authentication/TokenStoreExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SenseNet.Client.Authentication;
+
+// ReSharper disable once CheckNamespace
+namespace SenseNet.Extensions.DependencyInjection
+{
+    public static class TokenStoreExtensions
+    {
+        public static IServiceCollection AddSenseNetClientTokenStore(this IServiceCollection services)
+        {
+            services.AddSingleton<ITokenProvider, IdentityServerTokenProvider>();
+            services.AddSingleton<TokenStore>();
+
+            return services;
+        }
+    }
+}
