ddc196887a6a5ba122ec0f421471593ecdc7b7ac
tusmester <tusmester@gmail.com>
2020-11-04 14:06:44 +0100

    Let developers provide a client id along the secret when requesting an access token. (#52)

--- src/SenseNet.Client/Authentication/TokenStore.cs
+++ src/SenseNet.Client/Authentication/TokenStore.cs
@@ -20,7 +20,29 @@ namespace SenseNet.Client.Authentication
             _logger = logger;
         }
 
-        public async Task<string> GetTokenAsync(ServerContext server, string secret)
+        /// <summary>
+        /// Gets an access token from an authority. The authority is discovered from the content
+        /// repository service defined by the server parameter.
+        /// This method uses the client id returned by the discovery call from the repository.
+        /// If you have a client id, use the other overload.
+        /// </summary>
+        /// <param name="server">A server object that defines the content repository service.</param>
+        /// <param name="secret">A secret that is necessary for the token to be requested.</param>
+        /// <returns>An access token or null.</returns>
+        public Task<string> GetTokenAsync(ServerContext server, string secret)
+        {
+            return GetTokenAsync(server, null, secret);
+        }
+
+        /// <summary>
+        /// Gets an access token from an authority. The authority is discovered from the content
+        /// repository service defined by the server parameter.
+        /// </summary>
+        /// <param name="server">A server object that defines the content repository service.</param>
+        /// <param name="clientId">Client id necessary for the token request.</param>
+        /// <param name="secret">A secret that is necessary for the token to be requested.</param>
+        /// <returns>An access token or null.</returns>
+        public async Task<string> GetTokenAsync(ServerContext server, string clientId, string secret)
         {
             // look for the token in the cache
             var tokenCacheKey = "TK:" + server.Url;
@@ -34,6 +56,10 @@ namespace SenseNet.Client.Authentication
                 _logger?.LogTrace($"Getting authority info from {server.Url}.");
 
                 authInfo = await _tokenProvider.GetAuthorityInfoAsync(server).ConfigureAwait(false);
+
+                // set client id if provided
+                if (!string.IsNullOrEmpty(clientId))
+                    authInfo.ClientId = clientId;
 
                 if (!string.IsNullOrEmpty(authInfo.Authority))
                     _cache.Set(authInfoCacheKey, authInfo, TimeSpan.FromMinutes(30));
