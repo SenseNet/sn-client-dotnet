200aff9fec5e1289f7a947780b36ec67773b86ff
tusmester <tusmester@gmail.com>
2022-11-11 17:01:40 +0100

    Extend server factory API to handle custom tokens. (#74)

--- src/SenseNet.Client.Tests/ServerContextFactoryTests.cs
+++ src/SenseNet.Client.Tests/ServerContextFactoryTests.cs
@@ -58,6 +58,42 @@ namespace SenseNet.Client.Tests
             Assert.AreEqual(exampleUrl2, server2.Url);
         }
 
+        [TestMethod]
+        public async Task ServerContext_CustomToken()
+        {
+            const string exampleUrl1 = "https://example1.com";
+
+            var scf = GetFactory(services =>
+            {
+                services.ConfigureSenseNetRepository(opt =>
+                {
+                    opt.Url = exampleUrl1;
+                });
+            });
+
+            var server1 = await scf.GetServerAsync(token:"token1");
+            Assert.AreEqual("token1", server1.Authentication.AccessToken);
+        }
+        [TestMethod]
+        public async Task ServerContext_CustomToken_MultipleNamed()
+        {
+            const string exampleUrl1 = "https://example1.com";
+            const string exampleUrl2 = "https://example2.com";
+
+            var scf = GetFactory(services =>
+            {
+                services.ConfigureSenseNetRepository("x", opt => { opt.Url = exampleUrl1; });
+                services.ConfigureSenseNetRepository("y", opt => { opt.Url = exampleUrl2; });
+            });
+
+            var server1 = await scf.GetServerAsync("x", "token1");
+            var server2 = await scf.GetServerAsync("y", "token2");
+
+            Assert.AreEqual("token1", server1.Authentication.AccessToken);
+            Assert.AreEqual("token2", server2.Authentication.AccessToken);
+            Assert.AreNotEqual(server1.Url, server2.Url);
+        }
+
         private static IServerContextFactory GetFactory(Action<IServiceCollection> addServices)
         {
             var services = new ServiceCollection();
