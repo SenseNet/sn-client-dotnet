07b1c29ca62859689e27b754f777106e291611da
tusmester <tusmester@gmail.com>
2023-03-17 12:16:12 +0100

    Add demo mvc app. (#94)

---
+++ src/SenseNet.Client.DemoMvc/Controllers/ContentController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SenseNet.Client.DemoMvc.Models;
+
+namespace SenseNet.Client.DemoMvc.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly IRepositoryCollection _repositoryCollection;
+
+        public ContentController(IRepositoryCollection repositoryCollection)
+        {
+            _repositoryCollection = repositoryCollection;
+        }
+
+        public async Task<IActionResult> Index(int id = 0)
+        {
+            Content content;
+
+            var repository = await _repositoryCollection.GetRepositoryAsync(HttpContext.RequestAborted);
+
+            if (id == 0)
+            {
+                // display the root
+                content = await repository.LoadContentAsync("/Root/Content", HttpContext.RequestAborted);
+            }
+            else
+            {
+                // load the current content
+                content = await repository.LoadContentAsync(id, HttpContext.RequestAborted);
+            }
+
+            var children = await repository.LoadCollectionAsync(new LoadCollectionRequest
+            {
+                Path = content.Path
+            }, HttpContext.RequestAborted);
+
+            return View(new SnContent
+            {
+                Content = content,
+                Children = children
+            });
+        }
+    }
+}
---
+++ src/SenseNet.Client.DemoMvc/Models/SnContent.cs
@@ -0,0 +1,8 @@
+﻿namespace SenseNet.Client.DemoMvc.Models
+{
+    public class SnContent
+    {
+        public Content Content { get; set; }
+        public IEnumerable<Content> Children { get; set; }
+    }
+}
