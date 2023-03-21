8b8c47f3c4c5e7997e2293e881bc71b8999c9b75
Zoltán Gyebrovszki <kavics@sensenet.com>
2023-03-21 03:31:41 +0100

    Add tests for docs project.

--- src/SenseNet.Client.TestsForDocs/Infrastructure/ClientIntegrationTestBase.cs
+++ src/SenseNet.Client.TestsForDocs/Infrastructure/ClientIntegrationTestBase.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SenseNet.Diagnostics;
+using SenseNet.Extensions.DependencyInjection;
 
 namespace SenseNet.Client.TestsForDocs.Infrastructure
 {
@@ -126,5 +131,38 @@ namespace SenseNet.Client.TestsForDocs.Infrastructure
 
             return content;
         }
+
+        protected IRepositoryCollection GetRepositoryCollection(Action<IServiceCollection> addServices = null)
+        {
+            var services = new ServiceCollection();
+
+            services.AddLogging(builder =>
+            {
+                builder.AddConsole();
+                builder.SetMinimumLevel(LogLevel.Trace);
+            });
+
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true)
+                //.AddUserSecrets<ContentTests>()
+                .Build();
+
+            services
+                .AddSingleton<IConfiguration>(config)
+                .AddSenseNetClient()
+                //.AddSingleton<ITokenProvider, TestTokenProvider>()
+                //.AddSingleton<ITokenStore, TokenStore>()
+                .ConfigureSenseNetRepository("local", repositoryOptions =>
+                {
+                    // set test url and authentication in user secret
+                    config.GetSection("sensenet:repository").Bind(repositoryOptions);
+                });
+
+            addServices?.Invoke(services);
+
+            var provider = services.BuildServiceProvider();
+            return provider.GetRequiredService<IRepositoryCollection>();
+        }
+
     }
 }
