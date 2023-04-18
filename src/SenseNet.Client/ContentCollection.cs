9b81cce8390f479c41a50e3213a601fb7f7c2e34
tusmester <tusmester@gmail.com>
2023-04-18 08:59:24 +0200

    Minor xml docs changes.

--- src/SenseNet.Client/ContentCollection.cs
+++ src/SenseNet.Client/ContentCollection.cs
@@ -5,18 +5,18 @@ using System.Collections.Generic;
 namespace SenseNet.Client;
 
 /// <summary>
-/// Represents a set of <see cref="Content"/> for <c>LoadCollection</c> and <c>Query</c>... methods.
+/// Represents a set of <see cref="Content"/> for <c>LoadCollection</c> and <c>Query</c> methods.
 /// </summary>
 /// <typeparam name="T"></typeparam>
 public interface IContentCollection<out T> : IEnumerable<T> where T : Content
 {
     /// <summary>
-    /// Returns count of the current set without executing enumeration.
+    /// Returns the count of the current set without enumerating the collection.
     /// </summary>
     int Count { get; }
     /// <summary>
-    /// Gets all items in the defined collection if the request property InlineCount is InlineCountOptions.AllPages,
-    /// otherwise equals to Count.
+    /// Gets the count of all items in the collection if the request property
+    /// InlineCount was set to AllPages, otherwise equals to Count.
     /// </summary>
     int TotalCount { get; }
 }
@@ -36,8 +36,8 @@ public class ContentCollection<T> : IContentCollection<T> where T : Content
     /// Initializes a new <see cref="ContentCollection&lt;T&gt;"/> instance.
     /// </summary>
     /// <param name="items">Wrapped items.</param>
-    /// <param name="count">Count of the <paramref name="items"/>.</param>
-    /// <param name="totalCount">Total count of the collection.</param>
+    /// <param name="count">Count of the <paramref name="items"/> collection.</param>
+    /// <param name="totalCount">Total count of the collection, regardless of paging.</param>
     public ContentCollection(IEnumerable<T> items, int count, int totalCount)
     {
         _items = items;
