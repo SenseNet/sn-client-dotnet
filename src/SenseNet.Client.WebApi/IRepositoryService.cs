a3b1cd1ee8e075f6eb6d1c437f08935e93086a2f
tusmester <tusmester@gmail.com>
2023-07-10 13:29:59 +0200

    Add webapi project for server-side client scenarios.

---
+++ src/SenseNet.Client.WebApi/IRepositoryService.cs
@@ -0,0 +1,24 @@
+﻿namespace SenseNet.Client.WebApi
+{
+    public interface IRepositoryService
+    {
+        /// <summary>
+        /// Returns the repository that is configured to use the current user's token available in HttpContext.
+        /// </summary>
+        /// <param name="cancel">The token to monitor for cancellation requests.</param>
+        /// <returns>A task that wraps the repository instance.</returns>
+        public Task<IRepository> GetUserRepositoryAsync(CancellationToken cancel);
+        /// <summary>
+        /// Returns a repository without authentication.
+        /// </summary>
+        /// <param name="cancel">The token to monitor for cancellation requests.</param>
+        /// <returns>A task that wraps the repository instance.</returns>
+        public Task<IRepository> GetVisitorRepositoryAsync(CancellationToken cancel);
+        /// <summary>
+        /// Returns a repository that is configured to use the configured Admin user.
+        /// </summary>
+        /// <param name="cancel">The token to monitor for cancellation requests.</param>
+        /// <returns>A task that wraps the repository instance.</returns>
+        public Task<IRepository> GetAdminRepositoryAsync(CancellationToken cancel);
+    }
+}
---
+++ src/SenseNet.Client.WebApi/RepositoryService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace SenseNet.Client.WebApi
+{
+    public class RepositoryService : IRepositoryService
+    {
+        private readonly IRepositoryCollection _repositoryCollection;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ILogger<RepositoryService> _logger;
+
+        public RepositoryService(IRepositoryCollection repositoryCollection, IHttpContextAccessor httpContextAccessor,
+            ILogger<RepositoryService> logger)
+        {
+            _repositoryCollection = repositoryCollection;
+            _httpContextAccessor = httpContextAccessor;
+            _logger = logger;
+        }
+
+        public async Task<IRepository> GetUserRepositoryAsync(CancellationToken cancel)
+        {
+            var token = _httpContextAccessor.HttpContext?.GetBearerToken();
+            if (token != null)
+                return await GetRepositoryAsync(Repositories.UserRepository, token, cancel).ConfigureAwait(false);
+
+            _logger.LogTrace("Returning Visitor repository instead of User, because there is no token available.");
+
+            return await GetVisitorRepositoryAsync(cancel).ConfigureAwait(false);
+        }
+
+        public Task<IRepository> GetVisitorRepositoryAsync(CancellationToken cancel) =>
+            GetRepositoryAsync(Repositories.VisitorRepository, null, cancel);
+        public Task<IRepository> GetAdminRepositoryAsync(CancellationToken cancel) =>
+            GetRepositoryAsync(Repositories.AdminRepository, null, cancel);
+
+        private Task<IRepository> GetRepositoryAsync(string name, string? token, CancellationToken cancel) =>
+            _repositoryCollection.GetRepositoryAsync(new RepositoryArgs
+            {
+                Name = name,
+                AccessToken = token
+            }, cancel);
+    }
+}
