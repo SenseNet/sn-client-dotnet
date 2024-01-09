51285f379296d592cb0a12ce01e809904b1db5f5
Zoltán Gyebrovszki <kavics@sensenet.com>
2024-01-09 01:42:12 +0100

    User.Gender and MaritalStatus

--- src/SenseNet.Client.Tests/UnitTests/ContentGeneralPropertiesTests.cs
+++ src/SenseNet.Client.Tests/UnitTests/ContentGeneralPropertiesTests.cs
@@ -66,7 +66,6 @@ public class ContentGeneralPropertiesTests : TestBase
         Assert.IsNotNull(content.InheritableVersioningMode);
         Assert.AreEqual(VersioningMode.MajorOnly, content.InheritableVersioningMode);
     }
-
     [TestMethod]
     public async Task GeneralProps_T_Save_VersioningModes_Null()
     {
@@ -152,5 +151,333 @@ public class ContentGeneralPropertiesTests : TestBase
         Assert.AreEqual("Content1", data.Name.ToString());
         Assert.AreEqual("[\"1\"]", RemoveWhitespaces(data.VersioningMode.ToString()));
         Assert.AreEqual("[\"3\"]", RemoveWhitespaces(data.InheritableVersioningMode.ToString()));
+    }
+
+    [TestMethod]
+    public async Task GeneralProps_T_Load_ApprovingModes_Null()
+    {
+        // ALIGN
+        var restCaller = CreateRestCallerFor(@"{
+  ""d"": {
+    ""Id"": 999543,
+  }
+}");
+
+        var repositories = GetRepositoryCollection(services =>
+        {
+            services.AddSingleton(restCaller);
+            //services.RegisterGlobalContentType<TestContent_CustomProperties>();
+        });
+        var repository = await repositories.GetRepositoryAsync(FakeServer, CancellationToken.None)
+            .ConfigureAwait(false);
+
+        // ACT
+        var request = new LoadContentRequest { Path = "/Root/Content" };
+        var content = await repository.LoadContentAsync<Content>(request, CancellationToken.None);
+
+        // ASSERT
+        Assert.IsNull(content.ApprovingMode);
+        Assert.IsNull(content.InheritableApprovingMode);
+    }
+    [TestMethod]
+    public async Task GeneralProps_T_Load_ApprovingModes_NotNull()
+    {
+        // ALIGN
+        var restCaller = CreateRestCallerFor(@"{
+  ""d"": {
+    ""Id"": 999543,
+    ""ApprovingMode"": [ ""1"" ],
+    ""InheritableApprovingMode"": [ ""2"" ],
+  }
+}");
+
+        var repositories = GetRepositoryCollection(services =>
+        {
+            services.AddSingleton(restCaller);
+            //services.RegisterGlobalContentType<TestContent_CustomProperties>();
+        });
+        var repository = await repositories.GetRepositoryAsync(FakeServer, CancellationToken.None)
+            .ConfigureAwait(false);
+
+        // ACT
+        var request = new LoadContentRequest { Path = "/Root/Content" };
+        var content = await repository.LoadContentAsync<Content>(request, CancellationToken.None);
+
+        // ASSERT
+        Assert.IsNotNull(content.ApprovingMode);
+        Assert.AreEqual(ApprovingEnabled.No, content.ApprovingMode);
+        Assert.IsNotNull(content.InheritableApprovingMode);
+        Assert.AreEqual(ApprovingEnabled.Yes, content.InheritableApprovingMode);
+    }
+    [TestMethod]
+    public async Task GeneralProps_T_Save_ApprovingModes_Null()
+    {
+        // ALIGN
+        var restCaller = CreateRestCallerFor(@"{
+  ""d"": {
+    ""Id"": 999543,
+    ""Name"": ""Content1"",
+    ""ApprovingMode"": [ ""1"" ],
+    ""InheritableApprovingMode"": [ ""2"" ],
+  }
+}");
+
+        var repositories = GetRepositoryCollection(services =>
+        {
+            services.AddSingleton(restCaller);
+            //services.RegisterGlobalContentType<TestContent_CustomProperties>();
+        });
+        var repository = await repositories.GetRepositoryAsync(FakeServer, CancellationToken.None)
+            .ConfigureAwait(false);
+        var request = new LoadContentRequest { Path = "/Root/Content" };
+        var content = await repository.LoadContentAsync<Content>(request, CancellationToken.None);
+
+        // ACT
+        content.ApprovingMode = null;
+        content.InheritableApprovingMode = null;
+        await content.SaveAsync(_cancel);
+
+        // ASSERT
+        var calls = restCaller.ReceivedCalls().ToArray();
+        Assert.IsNotNull(calls);
+        Assert.AreEqual(3, calls.Length);
+        Assert.AreEqual("GetResponseStringAsync", calls[2].GetMethodInfo().Name);
+        var arguments = calls[2].GetArguments();
+        var json = (string)arguments[2]!;
+        json = json.Substring("models=[".Length).TrimEnd(']');
+        dynamic data = JsonHelper.Deserialize(json);
+        var dict = data.ToObject<Dictionary<string, object>>();
+        var keys = string.Join(", ", dict.Keys);
+        Assert.AreEqual("Name, ApprovingMode, InheritableApprovingMode", keys);
+        Assert.AreEqual("Content1", data.Name.ToString());
+    }
+    [TestMethod]
+    public async Task GeneralProps_T_Save_ApprovingModes_NotNull()
+    {
+        // ALIGN
+        var restCaller = CreateRestCallerFor(@"{
+  ""d"": {
+    ""Id"": 999543,
+    ""Name"": ""Content1"",
+    ""ApprovingMode"": [ ""1"" ],
+    ""InheritableApprovingMode"": [ ""0"" ],
+  }
+}");
+
+        var repositories = GetRepositoryCollection(services =>
+        {
+            services.AddSingleton(restCaller);
+            //services.RegisterGlobalContentType<TestContent_CustomProperties>();
+        });
+        var repository = await repositories.GetRepositoryAsync(FakeServer, CancellationToken.None)
+            .ConfigureAwait(false);
+        var request = new LoadContentRequest { Path = "/Root/Content" };
+        var content = await repository.LoadContentAsync<Content>(request, CancellationToken.None);
+
+        // ACT
+        content.ApprovingMode = ApprovingEnabled.Inherited;
+        content.InheritableApprovingMode = ApprovingEnabled.Yes;
+        await content.SaveAsync(_cancel);
+
+        // ASSERT
+        var calls = restCaller.ReceivedCalls().ToArray();
+        Assert.IsNotNull(calls);
+        Assert.AreEqual(3, calls.Length);
+        Assert.AreEqual("GetResponseStringAsync", calls[2].GetMethodInfo().Name);
+        var arguments = calls[2].GetArguments();
+        var json = (string)arguments[2]!;
+        json = json.Substring("models=[".Length).TrimEnd(']');
+        dynamic data = JsonHelper.Deserialize(json);
+        var dict = data.ToObject<Dictionary<string, object>>();
+        var keys = string.Join(", ", dict.Keys);
+        Assert.AreEqual("Name, ApprovingMode, InheritableApprovingMode", keys);
+        Assert.AreEqual("Content1", data.Name.ToString());
+        Assert.AreEqual("[\"0\"]", RemoveWhitespaces(data.ApprovingMode.ToString()));
+        Assert.AreEqual("[\"2\"]", RemoveWhitespaces(data.InheritableApprovingMode.ToString()));
+    }
+
+    [TestMethod]
+    public async Task GeneralProps_T_Gender_NullToNull()
+    {
+        await GenderTest(@"{ ""d"": { ""Id"": 999543}}", null, false, null);
+    }
+    [TestMethod]
+    public async Task GeneralProps_T_Gender_NullToNotDefined()
+    {
+        await GenderTest(@"{ ""d"": { ""Id"": 999543}}", Gender.NotDefined, true, "[\"...\"]");
+    }
+    [TestMethod]
+    public async Task GeneralProps_T_Gender_NullToFemale()
+    {
+        await GenderTest(@"{ ""d"": { ""Id"": 999543}}", Gender.Female, true, "[\"Female\"]");
+    }
+    [TestMethod]
+    public async Task GeneralProps_T_Gender_EmptyToNull()
+    {
+        await GenderTest(@"{ ""d"": { ""Id"": 999543,""Gender"": []}}", null, true, "");
+    }
+    [TestMethod]
+    public async Task GeneralProps_T_Gender_EmptyToFemale()
+    {
+        await GenderTest(@"{ ""d"": { ""Id"": 999543,""Gender"": []}}",
+            Gender.Female, true, "[\"Female\"]");
+    }
+    [TestMethod]
+    public async Task GeneralProps_T_Gender_NotDefinedToFemale()
+    {
+        await GenderTest(@"{ ""d"": { ""Id"": 999543,""Gender"": [""...""]}}",
+            Gender.Female, true, "[\"Female\"]");
+    }
+    [TestMethod]
+    public async Task GeneralProps_T_Gender_FemaleToNull()
+    {
+        await GenderTest(@"{ ""d"": { ""Id"": 999543,""Gender"": [""Female""]}}",
+            null, true, "");
+    }
+    [TestMethod]
+    public async Task GeneralProps_T_Gender_FemaleToNotDefined()
+    {
+        await GenderTest(@"{ ""d"": { ""Id"": 999543,""Gender"": [""Female""]}}",
+            Gender.NotDefined, true, "[\"...\"]");
+    }
+    [TestMethod]
+    public async Task GeneralProps_T_Gender_FemaleToMale()
+    {
+        await GenderTest(@"{ ""d"": { ""Id"": 999543,""Gender"": [""Female""]}}",
+            Gender.Male, true, "[\"Male\"]");
+    }
+    private async Task GenderTest(string loadString, Gender? editedValue, bool changed, string? expectedValueInSaveRequest)
+    {
+        // ALIGN
+        var restCaller = CreateRestCallerFor(loadString);
+
+        var repositories = GetRepositoryCollection(services =>
+        {
+            services.AddSingleton(restCaller);
+            //services.RegisterGlobalContentType<TestContent_CustomProperties>();
+        });
+        var repository = await repositories.GetRepositoryAsync(FakeServer, CancellationToken.None)
+            .ConfigureAwait(false);
+        var request = new LoadContentRequest { Path = "/Root/Content" };
+        var user = await repository.LoadContentAsync<User>(request, CancellationToken.None);
+
+        // ACT
+        user.Gender = editedValue;
+        user.InheritableApprovingMode = ApprovingEnabled.Yes;
+        await user.SaveAsync(_cancel);
+
+        // ASSERT
+        var calls = restCaller.ReceivedCalls().ToArray();
+        Assert.IsNotNull(calls);
+        Assert.AreEqual(3, calls.Length);
+        Assert.AreEqual("GetResponseStringAsync", calls[2].GetMethodInfo().Name);
+        var arguments = calls[2].GetArguments();
+        var json = (string)arguments[2]!;
+        json = json.Substring("models=[".Length).TrimEnd(']');
+        dynamic data = JsonHelper.Deserialize(json);
+        if (!changed)
+        {
+            Assert.IsFalse(json.Contains("Gender"));
+        }
+        else
+        {
+            Assert.IsTrue(json.Contains("Gender"));
+            Assert.AreEqual(expectedValueInSaveRequest, RemoveWhitespaces(data.Gender.ToString()));
+        }
+    }
+
+
+    [TestMethod]
+    public async Task GeneralProps_T_MaritalStatus_NullToNull()
+    {
+        await MaritalStatusTest(@"{ ""d"": { ""Id"": 999543}}",
+            null, false, null);
+    }
+    [TestMethod]
+    public async Task GeneralProps_T_MaritalStatus_NullToNotDefined()
+    {
+        await MaritalStatusTest(@"{ ""d"": { ""Id"": 999543}}",
+            MaritalStatus.NotDefined, true, "[\"...\"]");
+    }
+    [TestMethod]
+    public async Task GeneralProps_T_MaritalStatus_NullToSingle()
+    {
+        await MaritalStatusTest(@"{ ""d"": { ""Id"": 999543}}",
+            MaritalStatus.Single, true, "[\"Single\"]");
+    }
+    [TestMethod]
+    public async Task GeneralProps_T_MaritalStatus_EmptyToNull()
+    {
+        await MaritalStatusTest(@"{ ""d"": { ""Id"": 999543,""MaritalStatus"": []}}",
+            null, true, "");
+    }
+    [TestMethod]
+    public async Task GeneralProps_T_MaritalStatus_EmptyToSingle()
+    {
+        await MaritalStatusTest(@"{ ""d"": { ""Id"": 999543,""MaritalStatus"": []}}",
+            MaritalStatus.Single, true, "[\"Single\"]");
+    }
+    [TestMethod]
+    public async Task GeneralProps_T_MaritalStatus_NotDefinedToSingle()
+    {
+        await MaritalStatusTest(@"{ ""d"": { ""Id"": 999543,""MaritalStatus"": [""...""]}}",
+            MaritalStatus.Single, true, "[\"Single\"]");
+    }
+    [TestMethod]
+    public async Task GeneralProps_T_MaritalStatus_SingleToNull()
+    {
+        await MaritalStatusTest(@"{ ""d"": { ""Id"": 999543,""MaritalStatus"": [""Single""]}}",
+            null, true, "");
+    }
+    [TestMethod]
+    public async Task GeneralProps_T_MaritalStatus_SingleToNotDefined()
+    {
+        await MaritalStatusTest(@"{ ""d"": { ""Id"": 999543,""MaritalStatus"": [""Single""]}}",
+            MaritalStatus.NotDefined, true, "[\"...\"]");
+    }
+    [TestMethod]
+    public async Task GeneralProps_T_MaritalStatus_SingleToMarried()
+    {
+        await MaritalStatusTest(@"{ ""d"": { ""Id"": 999543,""MaritalStatus"": [""Single""]}}",
+            MaritalStatus.Married, true, "[\"Married\"]");
+    }
+    private async Task MaritalStatusTest(string loadString, MaritalStatus? editedValue, bool changed, string? expectedValueInSaveRequest)
+    {
+        // ALIGN
+        var restCaller = CreateRestCallerFor(loadString);
+
+        var repositories = GetRepositoryCollection(services =>
+        {
+            services.AddSingleton(restCaller);
+            //services.RegisterGlobalContentType<TestContent_CustomProperties>();
+        });
+        var repository = await repositories.GetRepositoryAsync(FakeServer, CancellationToken.None)
+            .ConfigureAwait(false);
+        var request = new LoadContentRequest { Path = "/Root/Content" };
+        var user = await repository.LoadContentAsync<User>(request, CancellationToken.None);
+
+        // ACT
+        user.MaritalStatus = editedValue;
+        user.InheritableApprovingMode = ApprovingEnabled.Yes;
+        await user.SaveAsync(_cancel);
+
+        // ASSERT
+        var calls = restCaller.ReceivedCalls().ToArray();
+        Assert.IsNotNull(calls);
+        Assert.AreEqual(3, calls.Length);
+        Assert.AreEqual("GetResponseStringAsync", calls[2].GetMethodInfo().Name);
+        var arguments = calls[2].GetArguments();
+        var json = (string)arguments[2]!;
+        json = json.Substring("models=[".Length).TrimEnd(']');
+        dynamic data = JsonHelper.Deserialize(json);
+        if (!changed)
+        {
+            Assert.IsFalse(json.Contains("MaritalStatus"));
+        }
+        else
+        {
+            Assert.IsTrue(json.Contains("MaritalStatus"));
+            Assert.AreEqual(expectedValueInSaveRequest, RemoveWhitespaces(data.MaritalStatus.ToString()));
+        }
     }
 }
--- src/SenseNet.Client/ContentTypes/User.cs
+++ src/SenseNet.Client/ContentTypes/User.cs
@@ -1,42 +1,117 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
+using System.Linq;
+using Newtonsoft.Json.Linq;
 
 // ReSharper disable once CheckNamespace
-namespace SenseNet.Client
+namespace SenseNet.Client;
+
+//UNDONE: Add doc
+public enum Gender { NotDefined, Female, Male }
+//UNDONE: Add doc
+public enum MaritalStatus { NotDefined, Single, Married }
+
+/// <summary>
+/// Represents a user in the sensenet repository.
+/// </summary>
+public class User : Content
 {
-    /// <summary>
-    /// Represents a user in the sensenet repository.
-    /// </summary>
-    public class User : Content
+    public User(IRestCaller restCaller, ILogger<Content> logger) : base(restCaller, logger)
     {
-        public User(IRestCaller restCaller, ILogger<Content> logger) : base(restCaller, logger)
+    }
+
+    public string LoginName { get; set; }
+    public string DisplayName { get; set; }
+    public string JobTitle { get; set; }
+    public bool Enabled { get; set; }
+    public string Domain { get; set; }
+    public string Email { get; set; }
+    public string FullName { get; set; }
+    public Image ImageRef { get; set; }
+    public Avatar Avatar { get; set; }
+    public DateTime LastSync { get; set; }
+    public User Manager { get; set; }
+    public string Department { get; set; }
+    public string Languages { get; set; }
+    public string Phone { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string TwitterAccount { get; set; }
+    public string FacebookURL { get; set; }
+    public string LinkedInURL { get; set; }
+    public string ProfilePath { get; set; }
+    public bool MultiFactorEnabled { get; set; }
+    public Gender? Gender { get; set; }
+    public MaritalStatus? MaritalStatus { get; set; }
+
+    protected override bool TryConvertToProperty(string propertyName, JToken jsonValue, out object propertyValue)
+    {
+        switch (propertyName)
         {
+            case nameof(Gender):
+            {
+                var arrayValue = jsonValue as JArray;
+                if (arrayValue != null && arrayValue.Count == 0)
+                {
+                    propertyValue = null;
+                    return true;
+                }
+                var stringValue = (arrayValue?.FirstOrDefault() as JValue)?.Value<string>();
+                if (Enum.TryParse<Gender>(stringValue, out var parsed))
+                {
+                    propertyValue = parsed;
+                    return true;
+                }
+                propertyValue = null;
+                return true;
+            }
+            case nameof(MaritalStatus):
+            {
+                var arrayValue = jsonValue as JArray;
+                if (arrayValue != null && arrayValue.Count == 0)
+                {
+                    propertyValue = Client.MaritalStatus.NotDefined;
+                    return true;
+                }
+                var stringValue = (arrayValue?.FirstOrDefault() as JValue)?.Value<string>();
+                if (Enum.TryParse<MaritalStatus>(stringValue, out var parsed))
+                {
+                    propertyValue = parsed;
+                    return true;
+                }
+                propertyValue = null;
+                return true;
+            }
+            default:
+                return base.TryConvertToProperty(propertyName, jsonValue, out propertyValue);
         }
-
-        public string LoginName { get; set; }
-        public string DisplayName { get; set; }
-        public string JobTitle { get; set; }
-        public bool Enabled { get; set; }
-        public string Domain { get; set; }
-        public string Email { get; set; }
-        public string FullName { get; set; }
-        public Image ImageRef { get; set; }
-        public Avatar Avatar { get; set; }
-        public DateTime LastSync { get; set; }
-        public User Manager { get; set; }
-        public string Department { get; set; }
-        public string Languages { get; set; }
-        public string Phone { get; set; }
-        public DateTime BirthDate { get; set; }
-        public string TwitterAccount { get; set; }
-        public string FacebookURL { get; set; }
-        public string LinkedInURL { get; set; }
-        public string ProfilePath { get; set; }
-        public bool MultiFactorEnabled { get; set; }
     }
 
-    public class Avatar
+    protected override bool TryConvertFromProperty(string propertyName, out object convertedValue)
     {
-        public string Url { get; set; }
+        switch (propertyName)
+        {
+            case nameof(Gender):
+                convertedValue = GenderOrMaritalStatusToStringArray(Gender?.ToString());
+                return true;
+            case nameof(MaritalStatus):
+                convertedValue = GenderOrMaritalStatusToStringArray(MaritalStatus?.ToString());
+                return true;
+            default:
+                return base.TryConvertFromProperty(propertyName, out convertedValue);
+        }
     }
+
+    private string[] GenderOrMaritalStatusToStringArray(string enumName)
+    {
+        if (enumName == null)
+            return null;
+        if (enumName == "NotDefined")
+            return new[] { "..." };
+        return new[] {enumName};
+    }
+}
+
+public class Avatar
+{
+    public string Url { get; set; }
 }
