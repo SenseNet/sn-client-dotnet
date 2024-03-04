c9eb53f4f9d7d9366499fb2e145cdda683dc32bb
Miklós Tóth <tusmester@gmail.com>
2024-03-04 09:13:10 +0100

    Add xml docs for options and extensions. (#133)

--- src/SenseNet.Client/RepositoryOptions.cs
+++ src/SenseNet.Client/RepositoryOptions.cs
@@ -3,9 +3,22 @@ using SenseNet.Extensions.DependencyInjection;
 
 namespace SenseNet.Client;
 
+/// <summary>
+/// Options for connecting to a sensenet repository.
+/// </summary>
 public class RepositoryOptions
 {
+    /// <summary>
+    /// The URL of the sensenet repository to connect to.
+    /// </summary>
     public string Url { get; set; }
+    /// <summary>
+    /// Authentication options for connecting to the repository.
+    /// </summary>
     public AuthenticationOptions Authentication { get; set; } = new AuthenticationOptions();
+    /// <summary>
+    /// Registered content types. Do not edit this directly, use the dedicated
+    /// extension methods for registering content types.
+    /// </summary>
     public RegisteredContentTypes RegisteredContentTypes { get; set; }
 }
